aefd2252590f5a58aa71388fadd1663b6f99e29a
rukshan-forg <138303187+rukshan-forg@users.noreply.github.com>
2024-06-19 12:18:20 +0530

    setup variables

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,16 +8,35 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+    final int[] x = new int[GAME_UNITS];
+    final int[] y = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten = 6;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
 
     GamePanel() {
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        Color Color;
+        this.setBackground(java.awt.Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
 
     }
 
+    @Override
     public void paintComponent(Graphics e) {
 
     }
